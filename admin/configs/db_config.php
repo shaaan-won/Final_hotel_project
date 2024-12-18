f62c4224d6c752a1c520723625cb481f3117d01f
shaaan-won <shawon.idb61@gmail.com>
2024-12-19 01:39:20 +0600

    First commit 1

--- admin/configs/db_config.php
+++ admin/configs/db_config.php
@@ -8,15 +8,15 @@
 
    //Local
    
-    //define("SERVER","localhost");
-    //define("USER","root");//rajib
-    //define("DATABASE","hosting");
-    //define("PASSWORD","");
+    // define("SERVER","localhost");
+    // define("USER","u391746232_hotel_db");//Hostinger database
+    // define("DATABASE","u391746232_hotel_db");
+    // define("PASSWORD","Shawon16520");
 
     //shawon
     define("SERVER","localhost");
     define("USER","root");//Sahwon
-    define("DATABASE","hotelmngmnt_shaan");
+    define("DATABASE","hotel_db");
     define("PASSWORD","");
 
     $db=new mysqli(SERVER,USER,PASSWORD,DATABASE);
