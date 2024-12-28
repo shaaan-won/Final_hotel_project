967881671dee8dd596f3c1a0d4f10e1183bae274
shaaan-won <shawon.idb61@gmail.com>
2024-12-28 21:05:49 +0600

    Finally done the primary work to upload in the shawon.site step 2

--- admin/header.php
+++ admin/header.php
@@ -53,8 +53,8 @@ $uid = $_SESSION["uid"];
      </style> -->
 
      <!-- for selct dropdown -->
-     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+     <link href="<?= $base_url ?>assets/css/select2.min.css" rel="stylesheet" />
+     <script src="<?= $base_url ?>assets/js/select2.min.js"></script>
 </head>
 
 <body>
